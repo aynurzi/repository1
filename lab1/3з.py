086d076c49941beab4ff66dbd7f0790d260a5bed
aynurzi <aynurzinnatullin87@gmail.com>
2025-06-16 09:52:41 +0300

    Лабораторная 1

---
+++ lab1/3з.py
@@ -0,0 +1,6 @@
+players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+midindex = len(players) // 2
+team_one = players[:midindex]
+team_two = players[midindex:]
+print(team_one)
+print(team_two)
